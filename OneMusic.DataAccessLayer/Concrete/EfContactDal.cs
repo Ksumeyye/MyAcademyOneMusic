ef1e1377b8eb815649bb8352814cd77b45bb6025
ksumeyye <sumeyyeekaya@outlook.com>
2024-07-24 19:03:37 +0300

    OneMusic entitieslerini luşturdum.

---
+++ OneMusic.DataAccessLayer/Concrete/EfContactDal.cs
@@ -0,0 +1,18 @@
+﻿using OneMusic.DataAccessLayer.Abstract;
+using OneMusic.DataAccessLayer.Context;
+using OneMusic.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneMusic.DataAccessLayer.Concrete
+{
+    public class EfContactDal : GenericRepository<IContactDal>
+    {
+        public EfContactDal(OneMusicContext context) : base(context)
+        {
+        }
+    }
+}
