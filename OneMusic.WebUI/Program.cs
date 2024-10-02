8bd6e739af720f7337ac27fe202aaead8803bc74
ksumeyye <sumeyyeekaya@outlook.com>
2024-10-02 23:44:49 +0300

    Filtreleme bakılacak

--- OneMusic.WebUI/Program.cs
+++ OneMusic.WebUI/Program.cs
@@ -1,6 +1,7 @@
 using FluentValidation;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
 using OneMusic.BusinessLayer.Abstract;
 using OneMusic.BusinessLayer.Concrete;
 using OneMusic.BusinessLayer.Validators;
@@ -13,16 +14,18 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// Programin katmanlar arasinda tanimladigimiz metotlari islemleri calistirabilmesi icin her bir katman nereden miras almiyorsa onu burda mutlaka tanimlamamiz gerekir.
+// Proje WebUI katmanindan calisiyordu ve program cs de bu katmaninin icerisindedir.
 
 builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<OneMusicContext>().AddErrorDescriber<CustomErrorDescriber>();
 
-builder.Services.AddScoped<IAboutDal, EfAboutDal>(); //IAboutDal içerisindeki methodlar EfAboutDal içerisine yazýlmýþtýr anlamýna gelir.(Registration iþlemidir bu.)
+builder.Services.AddScoped<IAboutDal, EfAboutDal>(); //IAboutDal icerisindeki methodlar EfAboutDal icerisine yazilmistir anlamina gelir Registration islemidir bu
 builder.Services.AddScoped<IAboutService,AboutManager>();
 
 builder.Services.AddScoped<IAlbumDal,EfAlbumDal>();
-builder.Services.AddScoped<IAlbumService,AlbumManager>();
+builder.Services.AddScoped<IAlbumService,AlbumManager>(); //BusinessLayer IAlbumService icerisindeki metotlar AlbumManager da yazilmistir (registiration islemi)
 
-builder.Services.AddScoped<IBannerDal,EfBannerDal>();
+builder.Services.AddScoped<IBannerDal, EfBannerDal>();
 builder.Services.AddScoped<IBannerService,BannerManager>();
 
 builder.Services.AddScoped<IContactDal, EfContactDal>();
@@ -42,17 +45,20 @@ builder.Services.AddScoped<ICategoryService,CategoryManager>();
 
 builder.Services.AddValidatorsFromAssemblyContaining<SingerValidator>();
 
-builder.Services.AddDbContext<OneMusicContext>();
+builder.Services.AddDbContext<OneMusicContext>();// DbContextimiz de OneMusicContext olarak belirtiriz Bunu vermezsek proje calismaz.
+
 builder.Services.AddControllersWithViews(option=>
 {
-    var authorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
-    option.Filters.Add(new AuthorizeFilter(authorizePolicy));
+    var authorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build(); //otantike olan bir kullanici gerekiyor authorize icin
+    option.Filters.Add(new AuthorizeFilter(authorizePolicy)); //Bu sekilde proje seviyesinde butun controllerlarimiza authorize eklemis olduk 
+    // fakat sadece burasiyla birakirsak loginle kayit olan tum sayfalara erisim kesilmis olur. Bu sorunun giderilmesi icin login ve register
+    // controllerlarina AllowAnonymous ekleriz ki onlara erisim saglayalim.
 });
 
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.LoginPath = "/Login/Index";
-    options.AccessDeniedPath = "/ErrorPage/AccessDenied";
+    options.LoginPath = "/Login/Index"; // bu islemi mvc de webconfig de yapiyorduk net core da ise program cs de yapiyoruz
+    options.AccessDeniedPath = "/ErrorPage/AccessDenied"; //yetkisi olmayan kisi bu sayfaya erisir
     options.LogoutPath= "/Login/Logout";
 });
 
@@ -65,7 +71,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404/", "?code{0}");
+app.UseStatusCodePagesWithReExecute("/ErrorPage/Error404/", "?code{0}"); // olmayan bir sayfaya erismeye calisinca
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -75,7 +81,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Default}/{action=Index}/{id?}"); //controller="Default" yaptýk. Çünkü giriþ yapýldýktan sonra ilk bu sayfaya gidilsin diye
+    pattern: "{controller=Default}/{action=Index}/{id?}"); //controller="Default" yaptik cunku giris yapildiktan sonra ilk bu sayfaya gidilsin diye
 
 app.UseEndpoints(endpoints =>
 {
